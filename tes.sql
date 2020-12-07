8b84665caf0146dcc71d6705840dea990642ddb3
Ignasius Leonardo <23135100+leonard04@users.noreply.github.com>
2020-12-07 10:21:58 +0700

    Update tes.sql

--- tes.sql
+++ tes.sql
@@ -5133,8 +5133,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `users_email_unique` (`email`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Dumping data for table cypher4.users: ~6 rows (approximately)
