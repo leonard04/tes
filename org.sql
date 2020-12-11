31e537a96a49c0a65b8aaa778abb23f70ab8503a
Ignasius Leonardo <23135100+leonard04@users.noreply.github.com>
2020-12-11 13:17:54 +0700

    Update org.sql

--- org.sql
+++ org.sql
@@ -1,20 +1,20 @@
 --marketing_clients
 --mti
 INSERT INTO `asset_wh` (`id`,`company_id`, `name`, `address`, `telephone`, `pic`) VALUES
-(NULL,23, 'Office Bintaro', 'Jalan Tegal Rotan no. 99, Bintaro', '021-74861508', 'Amelia');
+(NULL,23, 'Office Bintaro', 'Jalan Tegal Rotan no. 99, Bintaro', '021-74861508', 'Amelia'),
 (NULL,22, 'JARGAS SEMARANG', 'Jl. Muradi No.26, Kalibanteng Kulon, Kec. Semarang Bar., Kota Semarang, Jawa Tengah 50183', '', 'Iswidodo'),
 (NULL,22, 'JARGAS BLORA', 'Jln raya randu blatung -, menden km 0,5 kelurahan pilang rt 02 rw02', '', ''),
 (NULL,22, 'JARGAS SEMARANG BLORA', 'SEMARANG - BLORA JAWA TENGAH', '', ''),
 (NULL,22, 'VESSEL TOWER ', 'JL.BUNCIT RAYA NO 22A PEJATEN JAKSEL', '', ''),
-(NULL,22, 'PIJ', 'JL KETINTANG SELATAN NO. 79 SURABAYA ', '081331160700', 'FAJAR');
+(NULL,22, 'PIJ', 'JL KETINTANG SELATAN NO. 79 SURABAYA ', '081331160700', 'FAJAR'),
 (NULL,21, 'VESSEL TOWER', 'Jl. Warung Jati N0. 22A, Pejaten\r\nJakarta Selatan', '021. 7822544', 'Bpk. Arif'),
 (NULL,21, 'ASSET VESSEL WAREHOUSE & WORKSHOP', 'Jl. Jonggol - Cileungsi\r\nKp. Kubang, Ds. Cipeucang RT : 11/06 No.99\r\nCileungsi - Bogor', '021. 89932352', 'Bpk. Ari Pamudji'),
 (NULL,21, 'Genggam Asa Sekawan', 'Jalan Tegal Rotan Raya, No No.99, Pd. Jaya, Kec. Pd. Aren, Kota Tangerang Selatan, Banten 15220', '(021) 782257', 'Sugi'),
 (NULL,21, 'MAINTENANCE & OPERASI VESSEL WH ', 'Jl. Jonggol - Cileungsi Kp. Kubang, Ds. Cipeucang RT : 11/06 No.99 Cileungsi - Bogor', '', 'Gunawan'),
 (NULL,21, 'PROJECT PT.PSI ( DST & WELL TEST EN', 'Ds Pranten Gubug ,Grobogan Jawa Tengah ', '', ''),
 (NULL,21, 'FABRICATION VESSEL WAREHOUSE ', 'Jl. Jonggol - Cileungsi Kp. Kubang, Ds. Cipeucang RT : 11/06 No.99 Cileungsi - Bogor', '', 'DWI HANDOKO'),
-(NULL,21, 'GENERAL AFFAIR VESSEL WAREHOUSE ', 'Jl. Jonggol - Cileungsi Kp. Kubang, Ds. Cipeucang RT : 11/06 No.99 Cileungsi - Bogor 
-(NULL,21, 'BENGKEL ( SERVIS GENSET ) PAK ATMAD', 'jL Caringin no M.90 Bekasi\r\n', '081284443534', 'PAK ATMADI '),
+(NULL,21, 'GENERAL AFFAIR VESSEL WAREHOUSE ', 'Jl. Jonggol - Cileungsi Kp. Kubang, Ds. Cipeucang RT : 11/06 No.99 Cileungsi - Bogor', '', 'WIRJAM AGUS'),
+(NULL,21, 'BENGKEL ( SERVIS GENSET ) PAK ATMAD', 'jL Caringin no M.90 Bekasi', '081284443534', 'PAK ATMADI '),
 (NULL,21, 'RUMAH PUTIH', 'Jl Manunggal juang No 2 rt 04 Rw 07 Cilandak Cipete Selatan ', '', ''),
 (NULL,21, 'PT HEKSA INSTRUMENT SINERGI', 'JL RAYA LINGKAR SELATAN NO 10 KP SENGKOL RT 006 - RW 002 TANGERANG SELATAN BANTEN', '', 'FIDA ISMAIL'),
 (NULL,21, 'PT.TRITAMA ', 'Kawasan Industri Jababeka Jl. Jababeka XI B Block K10 ', '', 'Pak Dadan ');
