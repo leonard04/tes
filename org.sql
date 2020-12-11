3e7d049b7a1e95414eab8932a826363dd087a336
Ignasius Leonardo <23135100+leonard04@users.noreply.github.com>
2020-12-11 13:55:25 +0700

    Update org.sql

--- org.sql
+++ org.sql
@@ -1,10 +1,5 @@
 --treasure
-INSERT INTO `finance_treasure` (`id`, `company_id`,  `source`, `USD`, `IDR`, `account_usd`, `account_idr`, `type`, `br_balance`, `id_bank`, `currency`, `account_number`, `account_name`, `branch`, `address`, `bank_code`, `swift_code`, `actual_idr`, `actual_usd`) VALUES
-(NULL,21, 'BRI', 0.00, 29386043.80, 0.00, -208601530.06, 'bank', 0.00, NULL, NULL, NULL, 'PT BIMA ENERGI PERKASA', 'KALIBATA', NULL, NULL, NULL, 0.00, 0.00);
-(NULL,22, 'BANK MANDIRI KSO PIJ-PSI', 0.00, 5374161935.59, 0.00, 1182870781135.80, 'bank', 0.00, NULL, 'IDR', '127-00-1035165-6', 'Bank Mandiri Cabang Fatmawati', 'Cab.  Fatmawati', NULL, NULL, NULL, 4670747550.00, 0.00),
-(NULL,22, 'EMPTY BANK 2 USD', -4969.00, 69570969.00, 0.00, 0.00, 'bank', 0.00, NULL, NULL, NULL, 'JAKARTA2', 'JAKARTA2', NULL, NULL, NULL, 0.00, 0.00),
-(NULL,22, 'EMPTY BANK IDR 2', 0.00, 7799731.00, 0.00, 0.00, 'bank', 0.00, NULL, NULL, NULL, 'JAKARTA', 'JAKARTA', NULL, NULL, NULL, 0.00, 0.00);
-(NULL,23, 'MANDIRI ', 0.00, 807863309.37, 0.00, 1269552863211.00, 'bank', 0.00, NULL, NULL, '127-000-749-1325', 'MANDIRI MTI', 'KC JKT FATMAWATI', NULL, NULL, NULL, 0.00, 0.00);
+
 
 
 
