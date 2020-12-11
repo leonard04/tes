c06213c01aab4cff0a2685d7130c8f2d4dfc1f61
Ignasius Leonardo <23135100+leonard04@users.noreply.github.com>
2020-12-11 13:36:52 +0700

    Update org.sql

--- org.sql
+++ org.sql
@@ -1,41 +1,17 @@
---marketing_clients
---mti
-INSERT INTO `asset_wh` (`id`,`company_id`, `name`, `address`, `telephone`, `pic`) VALUES
-(NULL,23, 'Office Bintaro', 'Jalan Tegal Rotan no. 99, Bintaro', '021-74861508', 'Amelia'),
-(NULL,22, 'JARGAS SEMARANG', 'Jl. Muradi No.26, Kalibanteng Kulon, Kec. Semarang Bar., Kota Semarang, Jawa Tengah 50183', '', 'Iswidodo'),
-(NULL,22, 'JARGAS BLORA', 'Jln raya randu blatung -, menden km 0,5 kelurahan pilang rt 02 rw02', '', ''),
-(NULL,22, 'JARGAS SEMARANG BLORA', 'SEMARANG - BLORA JAWA TENGAH', '', ''),
-(NULL,22, 'VESSEL TOWER ', 'JL.BUNCIT RAYA NO 22A PEJATEN JAKSEL', '', ''),
-(NULL,22, 'PIJ', 'JL KETINTANG SELATAN NO. 79 SURABAYA ', '081331160700', 'FAJAR'),
-(NULL,21, 'VESSEL TOWER', 'Jl. Warung Jati N0. 22A, Pejaten\r\nJakarta Selatan', '021. 7822544', 'Bpk. Arif'),
-(NULL,21, 'ASSET VESSEL WAREHOUSE & WORKSHOP', 'Jl. Jonggol - Cileungsi\r\nKp. Kubang, Ds. Cipeucang RT : 11/06 No.99\r\nCileungsi - Bogor', '021. 89932352', 'Bpk. Ari Pamudji'),
-(NULL,21, 'Genggam Asa Sekawan', 'Jalan Tegal Rotan Raya, No No.99, Pd. Jaya, Kec. Pd. Aren, Kota Tangerang Selatan, Banten 15220', '(021) 782257', 'Sugi'),
-(NULL,21, 'MAINTENANCE & OPERASI VESSEL WH ', 'Jl. Jonggol - Cileungsi Kp. Kubang, Ds. Cipeucang RT : 11/06 No.99 Cileungsi - Bogor', '', 'Gunawan'),
-(NULL,21, 'PROJECT PT.PSI ( DST & WELL TEST EN', 'Ds Pranten Gubug ,Grobogan Jawa Tengah ', '', ''),
-(NULL,21, 'FABRICATION VESSEL WAREHOUSE ', 'Jl. Jonggol - Cileungsi Kp. Kubang, Ds. Cipeucang RT : 11/06 No.99 Cileungsi - Bogor', '', 'DWI HANDOKO'),
-(NULL,21, 'GENERAL AFFAIR VESSEL WAREHOUSE ', 'Jl. Jonggol - Cileungsi Kp. Kubang, Ds. Cipeucang RT : 11/06 No.99 Cileungsi - Bogor', '', 'WIRJAM AGUS'),
-(NULL,21, 'BENGKEL ( SERVIS GENSET ) PAK ATMAD', 'jL Caringin no M.90 Bekasi', '081284443534', 'PAK ATMADI '),
-(NULL,21, 'RUMAH PUTIH', 'Jl Manunggal juang No 2 rt 04 Rw 07 Cilandak Cipete Selatan ', '', ''),
-(NULL,21, 'PT HEKSA INSTRUMENT SINERGI', 'JL RAYA LINGKAR SELATAN NO 10 KP SENGKOL RT 006 - RW 002 TANGERANG SELATAN BANTEN', '', 'FIDA ISMAIL'),
-(NULL,21, 'PT.TRITAMA ', 'Kawasan Industri Jababeka Jl. Jababeka XI B Block K10 ', '', 'Pak Dadan ');
- 
-INSERT INTO `asset_wh` (`id`,`company_id`, `name`, `address`, `telephone`, `pic`, `deleted_at`, `deleted_by`) VALUES
-(NULL,1, 'LT. 3 ART', '', '082123028969', 'REGGY', NULL, NULL),
-(NULL,1, 'LT. 3 GAS', '', '', 'REZA', NULL, NULL),
-(NULL,1, 'LT. 4 PSI', '', '', 'MIRA', NULL, NULL),
-(NULL,1, 'LT. 5 PSI', '', '', 'MIRA',NULL, NULL),
-(NULL,1, 'LT. 5 MB', '', '', 'KAMAL', NULL, NULL),
-(NULL,1, 'BASEMENT MB', '', '', 'JUNI',NULL, NULL),
-(NULL,1, 'LT. 1 MB', '', '', 'JUNI', NULL, NULL),
-(NULL,1, 'LT. 2 MB', '', '', 'JUNI', NULL, NULL),
-(NULL,1, 'LT. 3 GUDANG MB', '', '', 'JUNI', NULL, NULL),
-(NULL,1, 'LT. 6 MB', '', '', 'JUNI', NULL, NULL),
-(NULL,1, 'VST TOWER', '', '', 'KAMAL', NULL, NULL),
-(NULL,1, 'VESSEL TOWER LT 2 STORAGE', 'JL. WARUNG JATI BARAT NO.22 A PEJATEN JAKARTA SELATAN', '081294158885', 'DEWI', NULL, NULL),
-(NULL,1, 'VESSEL TOWER LT 2 STORAGE RND', 'JL. WARUNG JATI BARAT NO.22 A PEJATEN JAKARTA SELATAN', '081294158885', 'Dewi', NULL, NULL),
-(NULL,1, 'RUMAH PUTIH', 'JLN MANUNGGAL JUANG NO 2 CILANDAK KOTA JAKARTA SELATAN KODE POS 12410', '', 'RU', NULL, NULL),
-(NULL,1, 'RUMAH CILANDAK ', '', '', '', NULL, NULL),
-(NULL,1, 'WAREHOUSE CILEUNGSI ', 'Jl. Raya Jonggol Kampung Kubang, desa Cipeucang, RT 11/06, No 99. Cileungsi', '-', 'Ari Pamudji', NULL, NULL),
-(NULL,1, 'VESSEL TOWER LT 4- OPERATION', 'Jl. Warung Jati No. 22 A Pejaten ', '513', 'DIDI KURNIADI', NULL, NULL);
+--util criteria
+INSERT INTO `finance_util_criteria` (`id`, `name`, `content`, `author`) VALUES
+(NULL,21, 'Utilisasi', 'Keperluan bulanan', 'ino'),
+(NULL,21, 'Perijinan', '', 'ino'),
+(NULL,21, 'Pajak', '', 'ino'),
+(NULL,1, 'Utilisasi', 'Keperluan bulanan', 'superuser'),
+(NULL,1, 'Perijinan', '', 'superuser'),
+(NULL,1, 'Peminjaman', 'deskripsi peminjaman', 'superuser'),
+(NULL,22, 'Utilisasi', 'Keperluan bulanan', 'ino'),
+(NULL,22, 'Perijinan', '', 'ino'),
+(NULL,22, 'Pajak', '', 'ino'),
+(NULL,23, 'Utilisasi', 'Keperluan bulanan', 'ino'),
+(NULL,23, 'Perijinan', '', 'ino'),
+(NULL,23, 'Pajak', '', 'ino');
+
 
 --end
