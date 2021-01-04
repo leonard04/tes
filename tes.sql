42b67f8d37d740baba88671b4ec5ae47d870fb67
Rafan <23135100+leonard04@users.noreply.github.com>
2021-01-04 15:56:23 +0700

    Update tes.sql

--- tes.sql
+++ tes.sql
@@ -1,1 +1,8 @@
 
+        Route::get('/general/sowaiting','AssetSreController@getSoWaiting')->name('so.waiting');
+        Route::get('/general/sobank','AssetSreController@getSoBank')->name('so.bank');
+        Route::get('/general/soreject','AssetSreController@getSoReject')->name('so.rejected');
+        
+        Route::get('/general/frwatings', 'AssetPreController@getFrWaiting')->name('fr.getFrWaiting');
+        Route::get('/general/frbanks', 'AssetPreController@getFrBank')->name('fr.getFrBank');
+        Route::get('/general/frrejects', 'AssetPreController@getFrReject')->name('fr.getFrReject');
