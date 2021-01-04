723908158ee3bb5b75507f1486a07062095b9776
Rafan <23135100+leonard04@users.noreply.github.com>
2021-01-04 18:08:36 +0700

    Update tes.sql

--- tes.sql
+++ tes.sql
@@ -1,8 +1,7 @@
 
-        Route::get('/general/sowaiting','AssetSreController@getSoWaiting')->name('so.waiting');
-        Route::get('/general/sobank','AssetSreController@getSoBank')->name('so.bank');
-        Route::get('/general/soreject','AssetSreController@getSoReject')->name('so.rejected');
+        Route::get('/hrd/getEmployee_data','HrdEmployeeController@getEmpGet')->name('employee.getdata');
+        Route::post('/hrd/getEmployee_data_post','HrdEmployeeController@getEmp')->name('employee.getdata_post');
         
-        Route::get('/general/frwatings', 'AssetPreController@getFrWaiting')->name('fr.getFrWaiting');
-        Route::get('/general/frbanks', 'AssetPreController@getFrBank')->name('fr.getFrBank');
-        Route::get('/general/frrejects', 'AssetPreController@getFrReject')->name('fr.getFrReject');
+        
+        
+        
