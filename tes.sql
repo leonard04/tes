effd036bf5ea01b81525840ef94311446d920bbe
Rafan <23135100+leonard04@users.noreply.github.com>
2021-01-05 11:09:59 +0700

    Update tes.sql

--- tes.sql
+++ tes.sql
@@ -1,3 +1,1 @@
-[{"currency": "IDR","amount": "403500000","IDR": "403500000"}] financing BMA
-[{"currency": "IDR","amount": "2000000000","IDR": "2000000000"}, {"currency": "IDR","amount": "1000000000","IDR": "1000000000"}, {"currency": "IDR","amount": "2207400000","IDR": "2207400000"}, {"currency": "IDR","amount": "32600000","IDR": "32600000"}, {"currency": "IDR","amount": "460000000","IDR": "460000000"}] APS
-[{"currency": "IDR","amount": "4540000000","IDR": "4540000000"}] gebang
+
